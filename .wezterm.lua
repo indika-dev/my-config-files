e1cc7a9fccac64426b94f98329378aeac12254b0
Stefan Maaßen <s.maassen@verband.creditreform.de>
2022-01-18 09:55:16 +0100

    added new shortcuts

--- .wezterm.lua
+++ .wezterm.lua
@@ -69,7 +69,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 end
 
 return {
-	add_wsl_distributions_to_launch_menu = false,
+	add_wsl_distributions_to_launch_menu = true,
 	font = wezterm.font("CaskaydiaCove NF"),
 	color_scheme = "Ayu Mirage",
 	default_prog = { "powershell.exe -NoLogo" },
@@ -82,28 +82,28 @@ return {
 			remote_address = "localhost:5000",
 		},
 	},
-    tab_bar_style = {
-        active_tab_left = wezterm.format({
-          {Background={Color="#0b0022"}},
-          {Foreground={Color="#2b2042"}},
-          {Text=SOLID_LEFT_ARROW},
-        }),
-        active_tab_right = wezterm.format({
-          {Background={Color="#0b0022"}},
-          {Foreground={Color="#2b2042"}},
-          {Text=SOLID_RIGHT_ARROW},
-        }),
-        inactive_tab_left = wezterm.format({
-          {Background={Color="#0b0022"}},
-          {Foreground={Color="#1b1032"}},
-          {Text=SOLID_LEFT_ARROW},
-        }),
-        inactive_tab_right = wezterm.format({
-          {Background={Color="#0b0022"}},
-          {Foreground={Color="#1b1032"}},
-          {Text=SOLID_RIGHT_ARROW},
-        }),
-  },
+	tab_bar_style = {
+		active_tab_left = wezterm.format({
+			{ Background = { Color = "#0b0022" } },
+			{ Foreground = { Color = "#2b2042" } },
+			{ Text = SOLID_LEFT_ARROW },
+		}),
+		active_tab_right = wezterm.format({
+			{ Background = { Color = "#0b0022" } },
+			{ Foreground = { Color = "#2b2042" } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}),
+		inactive_tab_left = wezterm.format({
+			{ Background = { Color = "#0b0022" } },
+			{ Foreground = { Color = "#1b1032" } },
+			{ Text = SOLID_LEFT_ARROW },
+		}),
+		inactive_tab_right = wezterm.format({
+			{ Background = { Color = "#0b0022" } },
+			{ Foreground = { Color = "#1b1032" } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}),
+	},
 	-- unix_domains = {
 	-- 	{
 	-- 		name = "local",
@@ -132,5 +132,27 @@ return {
 		},
 		{ key = "n", mods = "LEADER", action = "ShowTabNavigator" },
 		{ key = "l", mods = "LEADER", action = "ShowLauncher" },
+		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+		{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+		{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+		{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+		{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
 	},
 }
